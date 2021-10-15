a72a7979c1cdcdfe8d43105142af14d0043b3734
valeriyyemets <91984409+valeriyyemets@users.noreply.github.com>
2021-10-15 21:11:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,4 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-sudo curl -L "https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_ts_976c110733e7eff58704180706072907_archive.zip" -o ./html/20180706_novinano_ts_976c110733e7eff58704180706072907_archive.zip
-sudo curl -L "https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_ts_976c110733e7eff58704180706072907_installer.php" -o ./html/20180706_novinano_ts_976c110733e7eff58704180706072907_installer.php
-
-sudo chown -R 33.33 ./html
-
 sudo docker-compose up -d
