22dbd923b588c7be7994efad7e57554249882daf
valeriyyemets <91984409+valeriyyemets@users.noreply.github.com>
2021-10-15 21:58:38 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,5 +10,3 @@ sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-sudo docker-compose up -d
