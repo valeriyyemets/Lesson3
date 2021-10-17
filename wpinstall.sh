5a72712a36d1c6671cbd59352f33ef5a89afc865
valeriyyemets <91984409+valeriyyemets@users.noreply.github.com>
2021-10-17 10:36:21 +0300

    Update wpinstall.sh

--- wpinstall.sh
+++ wpinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -L "https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_ts_976c110733e7eff58704180706072907_archive.zip" -o /var/www/html/20180706_novinano_t
-curl -L "https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_ts_976c110733e7eff58704180706072907_installer.php" -o /var/www/html/20180706_novinano
+curl -L "https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_ts_976c110733e7eff58704180706072907_archive.zip" -o /var/www/html/20180706_novinano_ts_976c110733e7eff58704180706072907_archive.zip
+curl -L "https://github.com/mplesha/NoviNano/releases/download/v1.0/20180706_novinano_ts_976c110733e7eff58704180706072907_installer.php" -o /var/www/html/20180706_novinano_ts_976c110733e7eff58704180706072907_installer.php
 
 chown -R www-data:www-data /var/www/html
 
